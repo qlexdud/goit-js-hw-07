51a453c9ddf3df5d7c10bac53e48875399fb2bd8
qlexdud <133101510+qlexdud@users.noreply.github.com>
2025-01-28 23:51:41 +0200

    +1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const listGallery = document.querySelector(".gallery");
+const markup = galleryItems.map((elem) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" rel="noreferrer noopener" href="${elem.original}">
+            <img
+                class="gallery__image"
+                src="${elem.preview}"
+                data-source="${elem.original}"
+                alt="${elem.description}"
+            />
+        </a>
+    </li>`).join("");
 
+listGallery.insertAdjacentHTML("beforeend", markup);
+console.log(listGallery);
 console.log(galleryItems);
+
+listGallery.addEventListener("click", selectImg);
+
+function selectImg(event) {
+    const selectedImg = event.target.dataset.source;
+    console.log(selectedImg);
+
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src= "${selectedImg}"/>
+        </div>
+    `)
+    instance.show()
+}
+
+
+
+
